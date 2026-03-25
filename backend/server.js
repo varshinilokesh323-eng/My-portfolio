7819495d2f5fb8d12befb1b2e0c09908b5278227
Varshini <25bcae69@kristujayanti.com>
2026-03-25 20:43:33 +0530

    fixed port and route

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ app.use(bodyParser.json());
 
 app.use("/api", routes);
 
-const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
