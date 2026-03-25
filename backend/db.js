c9d44d5ac27d77dd49694afb0f8270b75e24bc0f
Varshini <25bcae69@kristujayanti.com>
2026-03-26 01:43:41 +0530

    fix ssl syntax error

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
   ssl: {
     rejectUnauthorized: false
   }
